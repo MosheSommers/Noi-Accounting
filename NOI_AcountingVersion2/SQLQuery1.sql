c248b7753ab458ce434a58b3f042bba6d35de548
Moshe SOmmers <moshesommers@gmail.com>
2017-02-22 17:00:07 -0500

    added user_info table

--- NOI_AcountingVersion2/SQLQuery1.sql
+++ NOI_AcountingVersion2/SQLQuery1.sql
@@ -1,10 +1,22 @@
---delete table company;
--- delete table my_transaction
+-- drop table my_transaction
+--drop table user_info
+--drop table company;
+
 create table company(
 	company_id int identity(1, 1),
 	name varchar(60) not null,
 
 	constraint pk_company_company_id primary key (company_id)
+);
+
+create table user_info(
+	user_id int identity(1, 1),
+	user_name varchar(30) unique,
+	password char(8) not null,
+	company_id int,
+
+	constraint pk_user_info_user_id primary key (user_id),
+	constraint fk_user_info_company foreign key (company_id) references company(company_id)
 );
 
 create table my_transaction(
@@ -14,7 +26,9 @@ create table my_transaction(
 	amount money not null,
 	date date,
 	company_id int not null,
+	user_id int,
 
 	constraint pk_my_transaction_transaction_id primary key (transaction_id),
-	constraint fk_my_transaction_company foreign key(company_id) references company(company_id)
+	constraint fk_my_transaction_company foreign key(company_id) references company(company_id),
+	constraint fk_my_transaction_user_info foreign key (user_id) references user_info(user_id)
 	);
