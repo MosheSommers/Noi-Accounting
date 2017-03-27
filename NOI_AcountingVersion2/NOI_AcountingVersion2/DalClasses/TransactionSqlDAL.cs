6267ce3e6bb313bd185fb7c9b34db53f36ae71b0
Moshe SOmmers <moshesommers@gmail.com>
2017-03-27 15:59:19 -0400

    COmpleted getTransaction Metyods in dal

--- NOI_AcountingVersion2/NOI_AcountingVersion2/DalClasses/TransactionSqlDAL.cs
+++ NOI_AcountingVersion2/NOI_AcountingVersion2/DalClasses/TransactionSqlDAL.cs
@@ -11,10 +11,12 @@ namespace NOIAcountingVersion2.DalClasses
 {
     public class TransactionSqlDAL : ITransactionDAL
     {
-        private const string InsertTransactionQuery = "insert into transaction values(@transactionType, @transactionName, @amount, @date, @companyId, @userId)";
-        private const string AllTransactionsQuery = "select * from transaction where company_id = @companyId";
-        private const string ExpensesQuery = "select * from transaction where comapany_id = @companyId and transaction_type = false";
-        private const string RevenueQuery = "select * from transaction where comapany_id = @companyId and transaction_type = true";
+        private const string InsertTransactionQuery = "insert into my_transaction values(@transactionType, @transactionName, @amount, @date, @companyId, @userId)";
+        private const string AllTransactionsQuery = "select * from my_transaction where company_id = @companyId";
+        private const string ExpensesQuery = "select * from my_transaction where comapany_id = @companyId and transaction_type = false";
+        private const string RevenueQuery = "select * from my_transaction where comapany_id = @companyId and transaction_type = true";
+        private const string ExepensesForTimePeriodQuery = "select * from my_transaction where date >= @start and date <= @end and transaction_type = false and company_id = @companyId";
+        private const string RevenueForTimePeriodQuery = "select * from my_transaction where date >= @start and date <= @end and transaction_type = true and company_id = @companyId";
         //private const string AllTrasactiosnQuery = "select from transaction where company_id = "
 
         private string connectionString;
@@ -60,39 +62,9 @@ namespace NOIAcountingVersion2.DalClasses
 
         public List<Transaction> GetCompanyTransactions(User u)
         {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    List<Transaction> trans = new List<Transaction>();
-                    connection.Open();
-                    SqlCommand command = new SqlCommand(AllTransactionsQuery, connection);
-                    command.Parameters.AddWithValue("@companyId", u.CompanyId);
-
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Transaction t = new Transaction();
-                        t.Amount = Convert.ToDouble(reader["amount"]);
-                        t.CompanyId = Convert.ToInt32(reader["company_id"]);
-                        t.Date = Convert.ToDateTime(reader["date"]);
-                        t.Description = Convert.ToString(reader["transaction_name"]);
-                        t.IsRevenue = Convert.ToBoolean(reader["transaction_type"]);
-                        t.TransactionId = Convert.ToInt32(reader["transaction_id"]);
-                        t.UserId = Convert.ToInt32(reader["user_id"]);
-
-                        trans.Add(t);
-                    }
-                    return trans;
-                }
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+            return GetTransactions(u, AllTransactionsQuery);
         }
-       
+
         public List<Transaction> GetCompanyExpenses(User u)
         {
             return GetTransactions(u, ExpensesQuery);
@@ -103,18 +75,18 @@ namespace NOIAcountingVersion2.DalClasses
             return GetTransactions(u, RevenueQuery);
         }
 
-        public List<Transaction> GetExpensesForTimePeriod(User u)
+        public List<Transaction> GetExpensesForTimePeriod(User u, TimePeriod t)
         {
-            return new List<Transaction>();
+            return GetTransactionsForTimePeriod(u, t, ExepensesForTimePeriodQuery);
+        }
+        
+        public List<Transaction> GetRevenueForTimePeriod(User u, TimePeriod t)
+        {
+            return GetTransactionsForTimePeriod(u, t, RevenueForTimePeriodQuery);
         }
 
-        public List<Transaction> GetRevenueForTimePeriod(User u)
-        {
-            return new List<Transaction>();
-        }
 
-
-        //Helper Method to query transactions from database and
+        //Helper Method to query transactions from database and return list of transaction
         private List<Transaction> GetTransactions(User u, string query)
         {
             try
@@ -159,6 +131,47 @@ namespace NOIAcountingVersion2.DalClasses
                 throw;
             }
 
+        }
+
+        private List<Transaction> GetTransactionsForTimePeriod(User u, TimePeriod t, string query)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@start", t.Start);
+                    command.Parameters.AddWithValue("@end", t.End);
+                    command.Parameters.AddWithValue("@companyId", u.CompanyId);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    List<Transaction> transactions = new List<Transaction>();
+
+                    while (reader.Read())
+                    {
+                        Transaction tran = new Transaction();
+                        tran.TransactionId = Convert.ToInt32(reader["transaction_id"]);
+                        tran.IsRevenue = Convert.ToBoolean(reader["transaction_type"]);
+                        tran.Description = Convert.ToString(reader["transaction_name"]);
+                        tran.Amount = Convert.ToDouble(reader["amount"]);
+                        tran.Date = Convert.ToDateTime(reader["date"]);
+                        tran.CompanyId = Convert.ToInt32(reader["company_id"]);
+                        tran.UserId = Convert.ToInt32(reader["user_id"]);
+
+                        transactions.Add(tran);
+                    }
+                    return transactions;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
     }
---
+++ NOI_AcountingVersion2/NOI_AcountingVersion2/ModelClasses/TimePeriod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOIAcountingVersion2.ModelClasses
+{
+    public class TimePeriod
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public TimePeriod(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
